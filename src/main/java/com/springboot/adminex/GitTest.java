cdfb40c63e098a53eff7d2a00112c78793c89e04
Asakaze <95083055+Youkari-17@users.noreply.github.com>
2023-03-26 22:35:31 +0800

    pull test

--- src/main/java/com/springboot/adminex/GitTest.java
+++ src/main/java/com/springboot/adminex/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("master 1");
         System.out.println("hot-fix 1");
         System.out.println("hot-fix 2");
+        System.out.println("pull test");
     }
 }
