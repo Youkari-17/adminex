2919bb990e5b2fbe3afb0c44a7d92fa0d7b416e0
root <2065324871@qq.com>
2023-03-26 18:29:50 +0800

    master commit 2

--- src/main/java/com/springboot/adminex/GitTest.java
+++ src/main/java/com/springboot/adminex/GitTest.java
@@ -3,5 +3,6 @@ package com.springboot.adminex;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hot-fix 1");
+        System.out.println("hot-fix 2");
     }
 }
